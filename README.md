5e19903b806e9f324830ce20b3a6c63028239634
Bruno Bertolini <bruno.bertolini@neoway.com.br>
2014-12-11 10:20:22 -0200

    [FEATURE] Replace templateUrl by html template string

---
+++ README.md
@@ -0,0 +1,73 @@
+# Gulp Directive Replace
+
+Replace templateUrl by minified html template
+
+## Installation
+
+npm install gulp-directive-replace
+
+## Usage
+
+```
+var gulp = require('gulp');
+var directiveReplace = require('gulp-directive-replace');
+
+gulp.task('directives', function(){
+
+  return gulp.src('./app/directives/*.js')
+    .pipe(directiveReplace())
+    .pipe(gulp.dest('./'));
+
+});
+```
+
+## Options 
+
+
+### root
+
+The path between directive templateUrl string, and gulpfile path.
+
+Example: 
+
+Let's say you have a following folder structure
+
+```
+/gulpfile.js
+/app
+    index.html
+    /directives
+        yourdirective.js
+    /partials
+        yourpartial.html
+```
+
+Then, your directive templateUrl is: '/partials/yourpartial.html'
+
+The plugin will get template url and needs find that there is a *app* folder, but will not. You need inform to it by "root" option, like this:
+
+```
+directiveReplace({root: 'app'})
+```
+
+### minify
+
+Html minifier options.
+
+This options is passed directly to [html-minifier](https://www.npmjs.com/package/html-minifier), and options documentation is [here](http://perfectionkills.com/experimenting-with-html-minifier/#options)
+
+The options default is:
+
+```
+{
+    root: '',
+    minify: {
+        removeComments: true,
+        collapseWhitespace:true
+    }
+}
+```
+
+## Credits
+
+This plugin is based on the [gulp-expand-template-url])(https://www.npmjs.com/package/gulp-expand-template-url)
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var fs      =  require('fs');
+var gutil   = require('gulp-util');
+var through = require('through2');
+var lodash  = require('lodash');
+var path    = require('path');
+var htmlMinifier    = require('html-minifier').minify;
+
+function templateTransformPlugin(options) {
+
+  var defaultOptions = {
+    root: '',
+    minify: {
+      removeComments: true,
+      collapseWhitespace:true
+    }
+  };
+
+  var opts = lodash.extend(defaultOptions, options);
+
+  return through.obj(objectStream);
+
+  function objectStream(file, enc, cb) {
+    /* jshint validthis: true */
+
+    var _this = this;
+
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      _this.emit('error', pluginError('Streaming not supported'));
+      return cb();
+    }
+
+    try {
+      var contents = file.contents.toString();
+      file.contents = new Buffer(transform(contents, opts));
+    } catch (err) {
+      err.fileName = file.path;
+      _this.emit('error', pluginError(err));
+    }
+
+    _this.push(file);
+    cb();
+  }
+}
+
+function pluginError(msg) {
+  return new gutil.PluginError('gulp-strip-line', msg);
+}
+
+function transform(content, opts) {
+
+  if (!content) {
+    return content;
+  }
+
+  var patt = /templateUrl\:[^\'\"]*(\'|\")([^\'\"]+)(\'|\")/gm;
+
+  var output = content.toString();
+  var templateUrlDirective = patt.exec(output);
+  var templateUrl = path.join(opts.root, templateUrlDirective[2]);
+  var template = fs.readFileSync( templateUrl , 'utf8');
+
+  template = htmlMinifier(template, opts.minify);
+
+  return output.replace(templateUrlRegExp, 'template: \'' + template + '\'');
+}
+
+module.exports = templateTransformPlugin;
